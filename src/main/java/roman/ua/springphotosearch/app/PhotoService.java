44213f03b05d5d332262b4243522304df6998f62
Kuryliak <makslolka7@gmail.com>
2022-05-08 14:20:07 +0200

    Connected H2 database,Created DownloadService and a bit other minor changes

--- src/main/java/roman/ua/springphotosearch/app/PhotoService.java
+++ src/main/java/roman/ua/springphotosearch/app/PhotoService.java
@@ -1,66 +1,42 @@
 package roman.ua.springphotosearch.app;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.server.ResponseStatusException;
 import roman.ua.springphotosearch.domain.Photo;
-
-import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.UUID;
 
 @Service
 public class PhotoService {
-    private HashMap<String, Photo> tempDb = new HashMap<>() {{
-        put("1", new Photo("1", "cat neko"));
-        put("2", new Photo("2", "ben dog"));
-        put("3", new Photo("3", "egor cat"));
-    }};
+    private PhotosRepository photosRepository;
 
-    public Collection<Photo> getPhotoList() {
-        return tempDb.values();
+    @Autowired
+    public PhotoService(PhotosRepository photosRepository) {
+        this.photosRepository = photosRepository;
     }
 
-    public Photo getPhotoById(String id) {
-        Photo photo = tempDb
-            .get(id);
-
-        if (photo == null) throw new
-            ResponseStatusException
-            (HttpStatus
-                .NOT_FOUND);
-
-        return photo;
+    public Iterable<Photo> getPhotoList() {
+        return photosRepository.findAll();
     }
 
-    public void removePhoto(String id) {
-        Photo photo = tempDb.remove(id);
-        if (photo == null) throw new
-            ResponseStatusException
-            (HttpStatus
-                .NOT_FOUND);
+    public Photo getPhotoById(Integer id) {
+        return photosRepository.findById(id).orElse(null);
+    }
+
+    public void removePhoto(Integer id) {
+        photosRepository.deleteById(id);
 
     }
 
     public Photo savePhoto(String fileName,
                            String contentType,
-                           byte[] data) throws IOException {
+                           byte[] data) {
         Photo photo = new Photo();
-        photo.setId(UUID
-            .randomUUID()
-            .toString());
 
         photo.setFileName(fileName);
         photo.setContentType(contentType);
         photo.setData(data);
 
-        tempDb.put(photo.getId(), photo);
+        photosRepository.save(photo);
 
         return photo;
     }
-
 }
